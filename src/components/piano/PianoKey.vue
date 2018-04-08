bb2bf8b0bdd4eca0a97c6ee9300b6664f9a7a081
Omar Jbara <omar.jbara2@gmail.com>
2018-04-08 04:36:39 +0200

    - Added a way to play while holding mouse

--- src/components/piano/PianoKey.vue
+++ src/components/piano/PianoKey.vue
@@ -1,8 +1,6 @@
 <template>
-    <div class="piano-key" :class="{'is-black-key': this.note.isBlackKey, 'is-playing': this.note.playing}">
-        <transition-group name="fade-piano-key" mode="in-out">
-            <div v-for="user in note.users" :key="user.id" class="piano-key-user" :style="{background: user.color, boxShadow: `0 0 50px ${user.color}`}"></div>
-        </transition-group>
+    <div class="piano-key" :class="{'is-black-key': note.isBlackKey, 'is-playing': note.playing}">
+            <div @mouseout="onMouseOut" v-for="user in note.users" :key="user.id" class="piano-key-user" :style="{background: user.color, boxShadow: `0 0 50px ${user.color}`}"></div>
     </div>
 
 </template>
@@ -18,6 +16,7 @@
         USER_RELEASE_SUSTAIN
     } from "../../store/modules/piano/actions";
     import {DELETE_ALL_KEYS_DOWN} from "../../store/modules/piano/mutations";
+    import { EventBus } from "../../main";
 
     const MAX_VELOCITY = 1
     const VELOCITY_STEPS = 127
@@ -75,14 +74,20 @@
                     this.midiAccess.startListening()
                 }
             },
+            onMouseEnter (e) {
+                if (e.buttons > 0 && !this.currentUserPlaying) {
+                    EventBus.$emit('note.release')
+                    this.play(0.5)
+                }
+            },
             onMouseDown() {
                 if (!this.currentUserPlaying) this.play(0.5)
             },
             onMouseOut() {
-                if (this.note.source === SOURCE_MOUSE && this.currentUserPlaying) this.release()
+                this.release()
             },
             onMouseUp() {
-                if (this.note.source === SOURCE_MOUSE && this.currentUserPlaying) this.release()
+                this.release()
             },
             ...mapActions('piano', [
                 USER_PLAY_NOTE,
@@ -105,23 +110,29 @@
             this.onMouseDown = this.onMouseDown.bind(this)
             this.onMouseUp = this.onMouseUp.bind(this)
             this.onMouseOut = this.onMouseOut.bind(this)
+            this.onMouseEnter = this.onMouseEnter.bind(this)
             this.blur = this.blur.bind(this)
             this.focus = this.focus.bind(this)
+            EventBus.$on('note.release', () => {
+                if (this.currentUserPlaying) {
+                    this.release()
+                }
+            })
             window.addEventListener('blur', this.blur)
             window.addEventListener('focus', this.focus)
             window.addEventListener('contextmenu', this.blur)
             this.$el.addEventListener('mousedown', this.onMouseDown)
             this.$el.addEventListener('mouseup', this.onMouseUp)
-            this.$el.addEventListener('mouseout', this.onMouseOut)
+            this.$el.addEventListener('mouseenter', this.onMouseEnter)
+            // this.$el.addEventListener('mouseout', this.onMouseOut)
         },
         beforeDestroy() {
             window.removeEventListener('blur', this.blur)
             window.removeEventListener('focus', this.focus)
             window.removeEventListener('contextmenu', this.blur)
             this.$el.removeEventListener('mousedown', this.onMouseDown)
-            this.$el.removeEventListener('mousedown', this.onMouseDown)
             this.$el.removeEventListener('mouseup', this.onMouseUp)
-            this.$el.removeEventListener('mouseout', this.onMouseOut)
+            // this.$el.removeEventListener('mouseout', this.onMouseOut)
         }
     }
 </script>
